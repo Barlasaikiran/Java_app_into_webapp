ab631ba38d4708ae088a6c5d58231c292fb9d9af
Saikiran <Saikiran@gmail.com>
2026-09-19 11:48:27 +0530

    keys removed

--- Root/Environments/Qa/backed.tf
+++ Root/Environments/Qa/backed.tf
@@ -3,6 +3,5 @@ terraform {
     resource_group_name = "backend-resource-group"
     storage_account_name = "backendstorageaccount123"
     container_name = "backend-container"
-    key = "qa-backend-2002"
   }
 }
