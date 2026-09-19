b136ae57739303233b26afdc7cb31bca7083bfe6
Saikiran <Saikiran@gmail.com>
2026-09-19 12:12:57 +0530

    Fixed backend

--- Root/Environments/Prod/backed.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name = "backend-resource-group"
-    storage_account_name = "backendstorageaccount123"
-    container_name = "backend-container"
-  }
-}
